ef47c1eff025fc9c8818ec20cde08fb3082493f9
0-daedalus <armageddon_WO@mail.ru>
2022-07-31 15:07:23 +0600

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    start = (b>a) ? a : b;
+    fin = (b>a) ? b : a;
+    if(typeof(a) != "number" || typeof(b) != "number" || a < 0 || b < 0) return "ERROR";
+    for(let i = start; i <= fin; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
