ab539a80c7ec24bd91864d7daedf297cc4a01243
0-daedalus <armageddon_WO@mail.ru>
2022-08-02 18:28:42 +0600

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0 || typeof(+a) != 'number') return 'OOPS';
+    let fib = [0, 1];
+    for(let i = 1; i <= a; i++){
+        fib[i+1] = fib[i-1]+fib[i];
+    }
+    return fib[a];
 };
 
 // Do not edit below this line
