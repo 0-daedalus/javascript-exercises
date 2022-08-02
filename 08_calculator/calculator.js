a24ce160e47e46d7a434c9c3a9002371e7cd6889
0-daedalus <armageddon_WO@mail.ru>
2022-08-02 18:03:19 +0600

    Complate 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((sum, arg) => sum+=arg, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((ans, arg) => ans*=arg, 1);
 };
 
-const power = function() {
-	
+const power = function(a, pow) {
+	return a**pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans = 1;
+  if (a === 0) return 1;
+	for(let i = a; i > 1; i--){
+    ans*=i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
