34d5c8a609dc8880de328c9607d519d902fd9981
0-daedalus <armageddon_WO@mail.ru>
2022-08-02 18:37:20 +0600

    Complete 12 with sort(), need to try reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort((p1, p2) => {
+        if(!p1.yearOfDeath) p1.yearOfDeath = (new Date()).getFullYear();
+        if(!p2.yearOfDeath) p2.yearOfDeath = (new Date()).getFullYear();
+        return p1.yearOfDeath-p1.yearOfBirth > p2.yearOfDeath-p2.yearOfBirth ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
